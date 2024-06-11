6c77979d3b3453be36448c6ab394cb17df8b78e1
luiizgcl <luiizgcl@gmail.com>
2024-06-11 19:32:02 -0300

    fix: error by php version

--- public/aulas/14/Veiculo.php
+++ public/aulas/14/Veiculo.php
@@ -1,59 +1,72 @@
 <?php
-class Veiculo {
+class Veiculo
+{
     private $nome;
     private $cor;
     private $ano;
     protected $botao;
-    
-    public function acelerar() {
-     if($this->botao == true){
-         echo "<p>O veiculo está acelerando.....</p>";
-     } else{
-         echo "<p>O veiculo está parado...</p>";
-     }   
+
+    public function acelerar()
+    {
+        if ($this->botao == true) {
+            echo "<p>O veiculo está acelerando.....</p>";
+        } else {
+            echo "<p>O veiculo está parado...</p>";
+        }
     }
-    
-    public function ligar(){
+
+    public function ligar()
+    {
         $this->botao = true;
     }
-    
-    public function desligar() {
+
+    public function desligar()
+    {
         $this->botao = false;
     }
 
-    public function getNome() {
+    public function getNome()
+    {
         return $this->nome;
     }
 
-    public function getCor() {
+    public function getCor()
+    {
         return $this->cor;
     }
 
-    public function getAno() {
+    public function getAno()
+    {
         return $this->ano;
     }
 
-    public function getBotao() {
+    public function getBotao()
+    {
         return $this->botao;
     }
 
-    public function setNome($nome): void {
+    public function setNome($nome)
+    {
         $this->nome = $nome;
     }
 
-    public function setCor($cor): void {
+    public function setCor($cor)
+    {
         $this->cor = $cor;
     }
 
-    public function setAno($ano): void {
+    public function setAno($ano)
+    {
         $this->ano = $ano;
     }
 
-    public function setBotao($botao): void {
+    public function setBotao($botao)
+    {
         $this->botao = $botao;
     }
 
-    public function __construct($n, $a, $b) {
+    public function __construct($n, $a, $b)
+    {
         $this->nome = $n;
         $this->cor = "Amarelo";
         $this->ano = $a;
@@ -61,4 +74,3 @@ class Veiculo {
         $this->desligar();
     }
 }
-?>
